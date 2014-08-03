5e553dce0f924b834fa90ba72bec6e8536fc8a73
David Desmaisons <desmaisons_david@hotmail.com>
2014-08-02 21:43:50 -0300

    rovi integration

---
+++ MusicCollection/ToolBox/DateTimeExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCollection.ToolBox
+{
+    public static class DateTimeExtension
+    {
+        public static long ToUnixTime(this DateTime value)
+        {
+            return (long)(Math.Truncate((value - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds));
+        }
+
+        public static long NowUtcUnixTime()
+        {
+            return DateTime.UtcNow.ToUnixTime();
+        }
+    }
+}
