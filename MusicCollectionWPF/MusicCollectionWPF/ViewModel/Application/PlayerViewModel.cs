6a0357ae80f9127b2f1935aa42816a104ca69ee4
David Desmaisons <desmaisons_david@hotmail.com>
2014-09-22 23:10:16 -0300

    Improve player view-model

--- MusicCollectionWPF/MusicCollectionWPF/ViewModel/Application/PlayerViewModel.cs
+++ MusicCollectionWPF/MusicCollectionWPF/ViewModel/Application/PlayerViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using MusicCollection.Fundation;
+using MusicCollection.Infra;
 using MusicCollectionWPF.ViewModelHelper;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,18 @@ namespace MusicCollectionWPF.ViewModel
     public class PlayerViewModel : ViewModelBase
     {
         private IMusicPlayer _IMusicPlayer;
-        public PlayerViewModel(IMusicPlayer iMusicPlayer)
+        private IAlbumPlayList _PlayList;
+        private IPlayListFactory _IPlayListFactory;
+        public PlayerViewModel(IMusicPlayer iMusicPlayer, IPlayListFactory iPlayListFactory)
         {
             _IMusicPlayer = iMusicPlayer;
+            _IPlayListFactory = iPlayListFactory;
+
+            _IMusicPlayer.TrackEvent += OnTrackEvent;
+            _IMusicPlayer.TrackPlaying += TrackPlaying;
+
+            _PlayList = _IPlayListFactory.CreateAlbumPlayList("Memory PlayList");
+            _IMusicPlayer.PlayList = _PlayList;
 
             Play = RelayCommand.Instanciate(()=>_IMusicPlayer.Mode = PlayMode.Play);
             Pause = RelayCommand.Instanciate(() => _IMusicPlayer.Mode = PlayMode.Paused);
@@ -22,6 +33,56 @@ namespace MusicCollectionWPF.ViewModel
             VolumeDown = RelayCommand.Instanciate(() => _IMusicPlayer.Volume -= 0.1);
             Like = RelayCommand.Instanciate(DoLike);
         }
+
+        private void OnTrackEvent(object sender, MusicTrackEventArgs TrackEvent)
+        {
+            switch (TrackEvent.What)
+            {
+                case TrackPlayingEvent.Loading:
+                    EndInMilliSeconds = TrackEvent.Track.Duration.TotalMilliseconds;
+                    CurentInMilliSeconds = 0;
+                    break;
+
+                case TrackPlayingEvent.BeginPlay:
+                    EndInMilliSeconds = _IMusicPlayer.MaxPosition.TotalMilliseconds;
+                    CurentInMilliSeconds = _IMusicPlayer.Position.TotalMilliseconds;
+                    break;
+
+                case TrackPlayingEvent.EndPlay:
+                    CurentInMilliSeconds = 0;
+                    break;
+
+                case TrackPlayingEvent.Broken:
+                    Trace.WriteLine("Media failed");
+                    CurentInMilliSeconds = 0;
+                    break;
+            }
+        }
+
+        private void TrackPlaying(object sender, MusicTrackPlayingEventArgs TrackEvent)
+        {
+            CurentInMilliSeconds = TrackEvent.Position.TotalMilliseconds;
+            EndInMilliSeconds = TrackEvent.MaxPosition.TotalMilliseconds;
+        }
+
+        public IAlbum CurrentPlaying { get { return this.Get<PlayerViewModel, IAlbum>(() => el => el._PlayList.CurrentAlbumItem); } }
+
+        public bool IsPlaying { get { return this.Get<PlayerViewModel, bool>(() => el => el._IMusicPlayer.Mode== PlayMode.Play); } }
+  
+        private double? _EndInMilliSeconds;
+        public double? EndInMilliSeconds
+        {
+            get { return _EndInMilliSeconds; }
+            set { Set(ref _EndInMilliSeconds, value); }
+        }
+
+        private double? _CurentInMilliSeconds;
+        public double? CurentInMilliSeconds
+        {
+            get { return _CurentInMilliSeconds; }
+            set { Set(ref _CurentInMilliSeconds, value); }
+        }
+
 
         public double Volume
         {
@@ -65,12 +126,38 @@ namespace MusicCollectionWPF.ViewModel
             _IMusicPlayer.Mode = PlayMode.Paused;
         }
 
+        private void AddAlbum(IEnumerable<IAlbum> Al)
+        {
+            Al.Apply(al => _PlayList.AddAlbum(al));
+        }
+
+
         public void AddAlbumAndPlay(IEnumerable<IAlbum> ialls)
         {
+            bool emptybefore = _PlayList.Albums.Count == 0;
+            AddAlbum(ialls);
+
+            IAlbum last = ialls.First();
+   
+            _PlayList.CurrentAlbumItem = last;
+
+            _IMusicPlayer.Mode = PlayMode.Play;
         }
 
         public void AddAlbumAndPlay(IEnumerable<ITrack> trcs)
         {
+            bool emptybefore = _PlayList.Albums.Count == 0;
+            var tr = trcs.Distinct();
+            var res = tr.Select(tra => tra.Album).Distinct();
+
+            AddAlbum(res);
+
+            var trlast = tr.First();
+            var lastalb = trlast.Album;
+
+            _PlayList.CurrentTrack = trlast;
+
+            _IMusicPlayer.Mode = PlayMode.Play;
         }
 
 
